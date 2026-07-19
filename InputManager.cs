3c2b54351e287a4482e21495a9b8e738e8b57d3c
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2026-07-19 22:43:32 +0200

    Create InputManager.cs

---
+++ InputManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using EasyButtons;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance {  get; private set; }
+    public static InputControls Actions;
+    [HideInInspector] public InputActionMap currentMap;
+    private InputActionMap globalMap;
+    public static event Action<InputActionMap> OnActionMapChange;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        Actions = new InputControls();
+        globalMap = Actions.GlobalAndDebug;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleActionMap(Actions.GridNavigation); // put in player state machine later.
+    }
+    public void ToggleActionMap(InputActionMap actionMap)
+    {
+        if (actionMap.enabled) return;
+
+        Actions.Disable();
+        globalMap.Enable();
+        OnActionMapChange?.Invoke(actionMap);
+        Debug.Log("Action map changed to: " +  actionMap.name);
+        currentMap = actionMap;
+        actionMap.Enable();
+        
+    }
+
+    public void DisableActions()
+    {
+        Actions.Disable();
+        OnActionMapChange?.Invoke(null);
+        Debug.Log("Action map disabled");
+    }
+    public void EnableActions()
+    {
+        ToggleActionMap(Actions.GridNavigation);
+        Debug.Log("Action map enabled");
+    }
+}
