1edb759a2845d442c150f08272f2ca798a3b8127
James <75171126+dashingtoadiee@users.noreply.github.com>
2026-07-23 00:34:26 +0200

    Rudimentary player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public int MovementSpeed;
+    public InputAction MoveAction;
 
+    private void OnEnable()
+    {
+        InputManager.OnActionMapChange += SetInputListeners;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnActionMapChange -= SetInputListeners;
+    }
+
+    #region Input Listeners
+    void SetInputListeners(InputActionMap actionMap)
+    {
+        UnsubscribeAllListeners();
+        if (actionMap != null)
+        {
+            switch (actionMap.name)
+            {
+                case nameof(InputManager.Actions.Player):
+                    MoveAction = InputManager.Actions.Player.Move;
+                    break;
+            }
+        }
+    }
+
+    void UnsubscribeAllListeners()
+    {
+        MoveAction = null;
+    }
+    #endregion
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,11 +46,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void FixedUpdate()
     {
-        
+        Debug.Log(MoveAction.ReadValue<Vector2>());
+        Vector3 moveTo = MoveAction.ReadValue<Vector2>().normalized * MovementSpeed * Time.fixedDeltaTime;
+        transform.position += moveTo;
     }
 }
