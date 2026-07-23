ef1a91529498b001a91508531171d758f18b3340
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2026-07-23 11:25:22 +0200

    card logic

--- Assets/Scripts/Cards/CardHolder.cs
+++ Assets/Scripts/Cards/CardHolder.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 public class CardHolder : MonoBehaviour
 {
     public Card Card;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 nextPos;
 
     public void Init(Card card)
     {
         Card = card;
+        nextPos = transform.position;
+    }
+
+    public void MoveToPosition(Vector3 pos)
+    {
+        nextPos = pos;
+    }
+
+    private void FixedUpdate()
+    {
+        if(nextPos.x - transform.position.x > 0.1f)
+        {
+            rb.MovePosition(nextPos);
+        }
     }
 }
