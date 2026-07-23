bf6ae999065f96f7e96e83ce84b57cf3920d0cd9
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2026-07-23 10:37:38 +0200

    fuck visual studio

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -4,10 +4,48 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public Camera cam;
+
+    private void OnEnable()
+    {
+        InputManager.OnActionMapChange += SetInputListeners;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnActionMapChange -= SetInputListeners;
+    }
+
+    #region Input Listeners
+    void SetInputListeners(InputActionMap actionMap)
+    {
+        UnsubscribeAllListeners();
+        if (actionMap != null)
+        {
+            switch (actionMap.name)
+            {
+                case nameof(InputManager.Actions.Player):
+                    MoveAction = InputManager.Actions.Player.MousePosition;
+                    InputManager.Actions.Player.Attack += TestRay;
+                    break;
+            }
+        }
+    }
+
+    void UnsubscribeAllListeners()
+    {
+        MoveAction = null;
+        InputManager.Actions.Player.Attack -= TestRay;
+    }
+    #endregion
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+    }
+    void TestRay()
+    {
+        Debug.Log(cam.ScreenPointToRay(Input.mousePosition));
     }
 
     // Update is called once per frame
@@ -18,4 +56,6 @@ public class PlayerManager : MonoBehaviour
             Debug.Log(cam.ScreenPointToRay(Input.mousePosition));
         }
     }
+
+    
 }
